0ad5f90458b308c54d78019435e182286c83ff9a
Maithreyan <maithreyan11@gmail.com>
2020-12-01 18:21:24 +0530

    code completed

--- day1/reversethearray.cpp
+++ day1/reversethearray.cpp
@@ -1,1 +1,22 @@
 #include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+void swap(char *a,char *b)
+{
+	char temp;;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+}
+int main()
+{
+	string str;
+	cout<<"Enter a string:"<<endl;
+	cin>>str;
+	ll n=str.size();
+	for(ll i=0;i<n/2;i++)
+	{
+		swap(&str[i],&str[n-i-1]);
+	}
+	cout<<str;
+}
