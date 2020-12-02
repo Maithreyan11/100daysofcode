84f4d326c30377805a5a4c3a7ab7aae6bd00602a
Maithreyan <maithreyan11@gmail.com>
2020-12-02 22:06:45 +0530

    shifting all the negative elements to the left side of the array

--- day2/negtoleft.cpp
+++ day2/negtoleft.cpp
@@ -1,1 +1,25 @@
 #include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+	ll n;
+	cout<<"Enter the size of the array:";
+	cin>>n;
+	ll a[n];
+	for(ll i=0;i<n;i++)
+	cin>>a[i];
+	ll j=0;
+	for(ll i=0;i<n;i++)
+	{
+		if(a[i]<0)
+		{
+			ll temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+			j++;
+		}
+	}
+	for(ll i=0;i<n;i++)
+	cout<<a[i]<<"\t";
+}
